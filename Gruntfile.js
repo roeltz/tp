b4c1148fd6ae60eca9774f6e6c249405e284ae3a
roeltz <leo_rothe@msn.com>
2014-10-08 06:41:18 -0430

    OCD with double quotes in Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,6 +12,6 @@ module.exports = function(grunt) {
 		}
 	});
 
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.registerTask('default', ['uglify']);
+	grunt.loadNpmTasks("grunt-contrib-uglify");
+	grunt.registerTask("default", ["uglify"]);
 };
